1c983784b625dba2c54687eb27ef667e33576971
Angel Torres <jatorresdev@gmail.com>
2016-09-25 13:04:53 -0500

    Ajuste Login

--- app/src/main/java/com/example/aprendiz/salesapp/LoginActivity.java
+++ app/src/main/java/com/example/aprendiz/salesapp/LoginActivity.java
@@ -28,21 +28,14 @@ import retrofit2.Response;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+    
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
+    private EditText mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
