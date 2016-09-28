7cb07fe7de4fa1168438d0a1812c1285e84d46e2
Angel Torres <jatorresdev@gmail.com>
2016-09-27 19:26:36 -0500

    Se eliminan comentarios

--- app/src/main/java/com/example/aprendiz/salesapp/fragments/LoginFragment.java
+++ app/src/main/java/com/example/aprendiz/salesapp/fragments/LoginFragment.java
@@ -54,31 +54,10 @@ public class LoginFragment extends Fragment {
     public LoginFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-//    public static LoginFragment newInstance(String param1, String param2) {
-//        LoginFragment fragment = new LoginFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
     }
 
     @Override
