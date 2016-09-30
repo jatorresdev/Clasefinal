07bf28fe9ee799bf313428205ec865554f95e64c
Angel Torres <jatorresdev@gmail.com>
2016-09-30 16:56:28 -0500

    Se cambia grilla de publicaciones a lista

--- app/src/main/java/com/example/aprendiz/salesapp/fragments/PublicationFragment.java
+++ app/src/main/java/com/example/aprendiz/salesapp/fragments/PublicationFragment.java
@@ -39,7 +39,7 @@ public class PublicationFragment extends Fragment {
     // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
     // TODO: Customize parameters
-    private int mColumnCount = 2;
+    private int mColumnCount = 1;
     private OnListFragmentInteractionListener mListener;
 
     /**
